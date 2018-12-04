e685a6f112c90e8904e284c1438ce8b6711bfe17
DerDerwish <derderwish@chaotikum.org>
2018-12-04 15:15:08 +0100

    IrobserverClient.java um Kommentare erweitert

--- src/IrobserverClient.java
+++ src/IrobserverClient.java
@@ -1,28 +1,42 @@
 public interface IrobserverClient
 {
+	// Verbinde zu Kombination aus Host & Port
+	// inklusive aller notwendiger Schritte
 	public boolean connect(
 		String host,
 		int port
 		);
+	// RobServer-Doku: GetRobot
+	public String getRobot();
+	// RobServer-Doku: GetVersion
 	public String getVersion();
+	// Verbindung sauber beenden
 	public boolean quit();
+	// RobServer-Doku: GetTimestamp
 	public string getTimestamp();
+	// RobServer-Doku: PingRobot
 	public int[] pingRobot(
 		int num,
 		int wait
 		);
+	// RobServer-Doku: CM_PING
 	public int ping();
+	// RobServer-Doku: SetVerbosity
 	public boolean setVerbosity(
 		int verbosity
 		);
+	// RobServer-Doku: SetAdeptSpeed
 	public boolean setAdeptSpeed(
 		int speed
 		);
+	// RobServer-Doku: SetAdeptAccel
 	public boolean setAdeptAccel(
 		int accelerate,
 		int brake
 		);
+	// RobServer-Doku: GetJointsMaxSpeed
 	public int[] getJointsMaxChange();
+	// RobServer-Doku: SetJointsMaxSpeed
 	public boolean setJointsMaxChange(
 		int alpha1,
 		int alpha2,
@@ -31,9 +45,11 @@ public interface IrobserverClient
 		int alpha5,
 		int alpha6
 		);
+	// RobServer-Doku: MoveRTHomRowWise
 	public boolean moveRTHomRowWise(
 		Matrix matrix
 		);
+	// RobServer-Doku: MoveRTHomRowWiseStatus
 	public boolean moveRTHomRowWiseStatus (
 		Matrix matrix,
 		boolean flip=true,
@@ -43,6 +59,8 @@ public interface IrobserverClient
 		boolean lefty=true,
 		boolean toggleArm=true
 		);
+	// RobServer-Doku: GetPositionHomRowWise
 	public Matrix getPositionHomRowWise();
+	// RobServer-Doku: GetStatus
 	public String[] getStatus();
 }
