64f868e6fbeeb487ee94b09bfaa38ee2f4245f56
DerDerwish <derderwish@chaotikum.org>
2018-12-11 11:54:47 +0100

    ItrackserverClient.java in src/model/ eingefuegt

---
+++ src/model/ItrackserverClient.java
@@ -0,0 +1,31 @@
+public interface ItrackserverClient
+{
+	// Verbindung zum Server
+	// Inklusive aller sinnvollen Intialisierungsanweisungen
+	public boolean connect(
+		String host,
+		int port
+		);
+	// TrackServer-Doku: CM_GETTRACKERS
+	public String[] getMarkers();
+	// TrackServer-Doku: Auswahl eines Trackers (Markers)
+	public boolean chooseMarker(
+		String marker
+		);
+	// TrackServer-Doku: Ausgabeformate der Trackerdaten
+	public boolean chooseOutputFormat(
+		String format
+		);
+	// TrackServer-Doku: CM_NEXTVALUE
+	public float[] getNextValues();
+	// TrackServer-Doku: CM PING
+	public boolean ping();
+	// TrackServer-Doku: CM SETLOGLEVEL
+	public boolean setLogLevel(
+		String level
+		);
+	// Sauberer Abbau der Verbindung
+	public boolean quit();
+	// TrackServer-Doku: CM GETREVISION
+	public String getVersion();
+}
