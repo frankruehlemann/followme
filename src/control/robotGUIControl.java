879a2e27140f63ff62e01ee982ff59bab0a4be6b
jorgenowak <jorge.nowak@student.uni-luebeck.de>
2018-12-14 00:53:42 +0100

    added more functions and a matrix view

--- src/control/robotGUIControl.java
+++ src/control/robotGUIControl.java
@@ -12,6 +12,8 @@ public class robotGUIControl {
 		
 		this.robgui.getRobConnect().setOnAction(this::handle);
 		this.robgui.getRobQuit().setOnAction(this::handle);
+		this.robgui.getRobSendMatrix().setOnAction(this::handle);
+		this.robgui.getRobCalibrate().setOnAction(this::handle);
 		
 	}
 	
@@ -25,8 +27,20 @@ public class robotGUIControl {
 			}
 		}
 		
+		if(event.getSource().equals(this.robgui.getRobCalibrate())) {
+			this.robgui.getCalib().calibrate();
+		}
+		
+		if(event.getSource().equals(this.robgui.getRobSendMatrix())) {
+			this.robgui.getMatrixView().updateMatrix();
+			this.robgui.getRobot().setAdeptSpeed(Double.parseDouble(this.robgui.getSpeedField().getText()));
+			this.robgui.getRobot().moveHomRowWise(this.robgui.getMatrixView().getMatrix());
+		}
+		
 		if(event.getSource().equals(this.robgui.getRobQuit())) {
+			this.robgui.clearInfo();
 			this.robgui.getRobot().quit();
 		}
+		
 	}
 }
