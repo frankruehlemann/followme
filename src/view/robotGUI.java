269f3147d175b30b4c90093d4d0cf2fedee514ff
jorgenowak <jorge.nowak@student.uni-luebeck.de>
2018-12-14 00:59:01 +0100

    added more stuff to gui

--- src/view/robotGUI.java
+++ src/view/robotGUI.java
@@ -7,25 +7,36 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.Text;
 import model.CustomOutputStream;
+import model.Kalibrierung;
+import model.Matrix;
 import model.Robot;
+import model.TrackingSystem;
 
+/**
+ * 
+ * @author OpfaH
+ *
+ */
 public class robotGUI extends GridPane{
 	
-	private Text cptRobName = new Text();
-	private Text cptRobType = new Text();
-	private Text cptRobServerVersion = new Text();
+	private TextField cptRobName = new TextField();
+	private TextField cptRobType = new TextField();
+	private TextField cptRobServerVersion = new TextField();
 	
-	private Text robIpadress = new Text();
-	private Text RobPort = new Text();
+	private TextField robIpadress = new TextField();
+	private TextField RobPort = new TextField();
 		
 	
 	private Button robConnect = new Button("Connect");
 	private Button robQuit = new Button("Quit");
+	private Button robSendMatrix = new Button("Send Matrix");
+	private Button robCalibrate = new Button("Calibrate");
 	
-	private TextField ipAddress= new TextField();
-	private TextField port= new TextField();
+	private TextField speed=new TextField("5");
+	
+	private TextField ipAddress= new TextField("127.0.0.1");
+	private TextField port= new TextField("5005");
 	
 	private TextField RobotName = new TextField();
 	private TextField RobotType= new TextField();
@@ -33,8 +44,16 @@ public class robotGUI extends GridPane{
 	
 	private Robot robot;
 	
+	private Kalibrierung calib;
+	
+	private MatrixView matrixView;
+	
 	private TextArea log = new TextArea();
 	
+	/**
+	 * 
+	 * @param robot
+	 */
 	public robotGUI(Robot robot) {
 		
 		this.robot = robot;
@@ -43,7 +62,14 @@ public class robotGUI extends GridPane{
 		this.RobotType.setEditable(false);
 		this.ServerVersion.setEditable(false);
 		
+		this.calib = new Kalibrierung(new TrackingSystem(),this.robot,this);
+		this.calib.setMeasureCount(10);
+		
+		this.setHgap(10);
+		this.setVgap(10);
 		this.setPadding(new Insets(15));
+		
+		//*************************************************************************************************
 		
 		this.add(new Label("Robot Name"), 0, 0);
 		this.add(this.RobotName, 1, 0);
@@ -51,6 +77,8 @@ public class robotGUI extends GridPane{
 		this.add(this.RobotType, 1, 1);
 		this.add(new Label("Server Version"), 0, 2);
 		this.add(this.ServerVersion, 1, 2);
+		
+		//*************************************************************************************************
 		
 		this.add(new Label("IP-Address"), 4, 0);
 		this.add(this.ipAddress, 5, 0);
@@ -60,34 +88,64 @@ public class robotGUI extends GridPane{
 		this.add(this.robConnect, 4, 2);
 		this.add(this.robQuit, 5, 2);
 		
+		this.add(this.robCalibrate, 4, 3);
+		//*************************************************************************************************
+		
+		this.add(this.robSendMatrix, 0, 5);
+		this.add(new Label("Speed"), 1, 5);
+		this.add(this.speed, 2, 5);
+		Matrix matrix = new Matrix(new double[] {1,0,0,200,0,1,0,300,0,0,1,500},4);
+		this.matrixView = new MatrixView(matrix);
+		this.add(this.matrixView, 0, 6,5,3);
+		
+		//*************************************************************************************************
 		log.setEditable(false);
-		this.add(this.log, 6, 0,1,7);
+		this.add(this.log, 6, 0,1,10);
 		
 		PrintStream ps = new PrintStream(new CustomOutputStream(this.log));
 		System.setOut(ps);
 		System.setErr(ps);
 	}
-	
+	/**
+	 * 
+	 */
 	public void gatherInformation() {
 		this.RobotName.setText(this.robot.getRobot());
 		this.RobotType.setText(this.robot.getRobType());
 		this.ServerVersion.setText(this.robot.getVersion());
 		
 	}
-	
+	/**
+	 * 
+	 */
+	public void clearInfo() {
+		this.RobotName.clear();
+		this.RobotType.clear();
+		this.ServerVersion.clear();
+	}
+	/**
+	 * 
+	 * @return
+	 */
 	public Robot getRobot() {
 		return this.robot;
 	}
-	
-	public Text getCptRobName() {
+	/**
+	 * 
+	 * @return
+	 */
+	public TextField getCptRobName() {
 		return cptRobName;
 	}
-
-	public Text getCptRobType() {
+	/**
+	 * 
+	 * @return
+	 */
+	public TextField getCptRobType() {
 		return cptRobType;
 	}
 
-	public Text getCptRobServerVersion() {
+	public TextField getCptRobServerVersion() {
 		return cptRobServerVersion;
 	}
 
@@ -99,11 +157,11 @@ public class robotGUI extends GridPane{
 		return this.robConnect;
 	}
 
-	public Text getRobIpadress() {
+	public TextField getRobIpadress() {
 		return robIpadress;
 	}
 
-	public Text getRobPort() {
+	public TextField getRobPort() {
 		return RobPort;
 	}
 
@@ -114,7 +172,21 @@ public class robotGUI extends GridPane{
 	public TextField getPort() {
 		return port;
 	}
-	
-	
-	
+
+	public Button getRobSendMatrix() {
+		return robSendMatrix;
+	}
+
+	public MatrixView getMatrixView() {
+		return this.matrixView;
+	}
+	public TextField getSpeedField() {
+		return this.speed;
+	}
+	public Button getRobCalibrate() {
+		return robCalibrate;
+	}
+	public Kalibrierung getCalib() {
+		return this.calib;
+	}
 }
