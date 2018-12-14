4be807dd90cf82b5c7c9ae1d78aa24d6ce2a0865
jorgenowak <jorge.nowak@student.uni-luebeck.de>
2018-12-14 12:37:36 +0100

    Nothing important

--- src/main/main.java
+++ src/main/main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import model.Matrix;
 import model.Robot;
 import view.robotGUI;
 
